091aaf65b733207af14dc4076b7d939f9d040419
Samuel <sollysamuel12@gmail.com>
2019-01-10 04:43:10 +0100

    new

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<title>SVG</title>
+</head>
+<body>
+<style>
+body {
+background: red;
+margin: 0 auto;
+padding: none;
+overflow-x : hidden;
+
+}
+
+#svgicon {
+ width: 100%;
+ height: max-height;
+	
+}
+</style>
+<div id="svgicon">
+
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"   height="auto" viewBox="0 0 1920 1080">
+  <defs>
+    <clipPath id="clip-Web_1920_1">
+      <rect width="1920" height="1080"/>
+    </clipPath>
+  </defs>
+  <g id="Web_1920_1" data-name="Web 1920 – 1" clip-path="url(#clip-Web_1920_1)">
+    <rect width="1920" height="1080" fill="#5e785b"/>
+    <path id="Path_3" data-name="Path 3" d="M1920,0V1080H0Z" fill="#3b3a40"/>
+    <path id="Path_1" data-name="Path 1" d="M418.024-28.275s169.907,243.8,25.646,322.872-160.29,207.561-160.29,207.561,54.5,148.258-112.2,135.079S-38,597.7-107.726,584.523-86.087,95.273-78.874-67.81,418.024-28.275,418.024-28.275Z" fill="#44427e"/>
+    <text id="Hello" transform="translate(730 623)" fill="#cfc8c8" font-size="200" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">Hello</tspan></text>
+    <path id="Path_2" data-name="Path 2" d="M1943.2,620.665s-174.018,165.317-174.018,174.018,82.659,219.7,110.212,292.931,98.61,0,98.61,0Z" fill="#8f6262"/>
+  </g>
+</svg>
+
+</div>
+<h1> Hello </h1>
+</body>
+</html>
